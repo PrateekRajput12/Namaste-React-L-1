a9844784a16b28a6f3a1d5b8e88d7d56a550538a
PrateekRajput12 <ficredinn@gmail.com>
2025-02-10 21:10:13 +0530

    using react

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let root = document.getElementById("root")
+// let root = document.getElementById("root")
 
-let h1 = document.createElement("h1")
-h1.innerText = "Namaste"
+// let h1 = document.createElement("h1")
+// h1.innerText = "Namaste"
 
-root.appendChild(h1)
+// root.appendChild(h1)
+
+const h1 = React.createElement("h1", {}, "Namaste React")
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(h1)
+
